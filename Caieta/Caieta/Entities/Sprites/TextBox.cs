d5bcb1d1c5ca328602664ebb2a424be92555fb6b
marcelomesmo <mcelobb@gmail.com>
2019-04-23 23:12:32 -0300

    Added TextBox concept and Fade (In+Out)
    - Added TextBox entity concept to create Text with imbued Colliders.
    - Added Fade (FadeIn+Out) with in-between wait time.
    Ex.: Can now create fades with Fade(in_time, wait_time, out_time, Action).

---
+++ Caieta/Caieta/Entities/Sprites/TextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Caieta.Components.Renderables.Text;
+using Caieta.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Caieta
+{
+    public class TextBox : Entity
+    {
+        public Text Text;
+        public BoxCollider Collider;
+
+        public TextBox(string entityname, string content, string font = "", bool initial_visibility = true) : base(entityname, initial_visibility)
+        {
+            _content = content;
+            _fontName = font;
+        }
+
+        private Vector2 _textSize;
+        private string _content, _fontName;
+
+        public override void Create()
+        {
+            base.Create();
+
+            Text = new Text(_content, _fontName);
+            Add(Text);
+
+            _textSize = Text.Font.MeasureString(_content);
+            Collider = new BoxCollider(_textSize.X, _textSize.Y);
+            Add(Collider);
+
+            Get<Text>().Align(Collider);
+        }
+
+    }
+}
---
+++ Caieta/Caieta/Scenes/Transitions/Fade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Caieta.Components.Utils;
+using Microsoft.Xna.Framework;
+
+namespace Caieta.Scenes.Transitions
+{
+    public class Fade : Transition
+    {
+        private Color _FadeInColor;
+        private Color _FadeOutColor;
+        private float _WaitTime;
+        private float _OutTime;
+
+        Action OnFadeFinish;
+
+        // Default is InTimer;
+        Timer WaitTimer;
+        Timer OutTimer;
+        private int _OutProgress;
+
+        public Fade(float in_time, float wait_time, float out_time, Color fadeInColor, Color fadeOutColor, Action OnFinish = null, string entityname = "Fade", bool initial_visibility = true) : base (in_time, entityname, initial_visibility)
+        {
+            _FadeInColor = fadeInColor;
+            _FadeOutColor = fadeOutColor;
+            _WaitTime = wait_time;
+            _OutTime = out_time;
+
+            _OutProgress = 0;
+
+            OnFadeFinish = OnFinish;
+        }
+        public Fade(float in_time, float wait_time, float out_time, Action OnFinish = null, string entityname = "Fade", bool initial_visibility = true) : this(in_time, wait_time, out_time, Color.Black, Color.Black, OnFinish, entityname, initial_visibility){ }
+
+        public override void Create()
+        {
+            base.Create();
+
+            WaitTimer = new Timer(_WaitTime);
+            Add(WaitTimer);
+
+            OutTimer = new Timer(_OutTime);
+            Add(OutTimer);
+
+            // Fade In Finish
+            OnFinish = WaitTimer.Start;
+            WaitTimer.OnTime = OutTimer.Start;
+            OutTimer.OnTime = OnFadeFinish;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (Progress < 100)
+                Progress = Timer.TargetTime == 0f ? 100 : (int)((Timer.ElapsedTime / Timer.TargetTime) * 100f);
+
+            if(OutTimer.IsRunning)
+                _OutProgress = OutTimer.TargetTime == 0f ? 100 : (int)((OutTimer.ElapsedTime / OutTimer.TargetTime) * 100f);
+        }
+
+        public override void Render()
+        {
+            base.Render();
+
+            if(Progress < 100)
+                Graphics.DrawRect(new Rectangle((int)Engine.SceneManager.Camera.X, (int)Engine.SceneManager.Camera.Y, Graphics.ViewWidth, Graphics.ViewHeight), _FadeInColor, (100 - Progress), FillType.FILL);
+
+            if (OutTimer.IsRunning && _OutProgress < 100)
+                Graphics.DrawRect(new Rectangle((int)Engine.SceneManager.Camera.X, (int)Engine.SceneManager.Camera.Y, Graphics.ViewWidth, Graphics.ViewHeight), _FadeOutColor, _OutProgress, FillType.FILL);
+
+        }
+    }
+}
